0fd2c61579a19a1481741b9de73461e4e991c361
Anjani  Kumari <anjanikumari@Anjanis-MacBook-Air.local>
2020-09-12 07:38:03 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express"),
+    app     = express(),
+    bodyParser = require("body-parser");
+
+    app.set("view engine", "ejs");
+    app.get("/", function(req,res){
+        res.render(home);
+    });
+    
+    app.listen(process.env.PORT, process.env.IP, function(){
+        console.log("Server is running!");
+    });
+
